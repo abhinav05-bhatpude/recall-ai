f182ebfd515a25c25b5731d9670281f727a41d60
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-06-21 14:24:08 -0700

    feat: setup nextauth google provider

--- recall-ai/src/auth.ts
+++ recall-ai/src/auth.ts
@@ -1,23 +1,12 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-import { PrismaAdapter } from "@auth/prisma-adapter";
-
-import { prisma } from "@/lib/prisma";
-
 export const { handlers, auth, signIn, signOut } =
   NextAuth({
-    adapter: PrismaAdapter(prisma),
-
     providers: [
       Google({
         clientId: process.env.AUTH_GOOGLE_ID!,
-        clientSecret:
-          process.env.AUTH_GOOGLE_SECRET!,
+        clientSecret: process.env.AUTH_GOOGLE_SECRET!,
       }),
     ],
-
-    session: {
-      strategy: "database",
-    },
   });
